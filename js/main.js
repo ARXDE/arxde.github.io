4aa094f19a9867ad8f0a610930a0ef74d11998a4
ARXDE <cosmultigizer@gmail.com>
2024-09-11 21:52:50 +0300

    copyright 2024 and trademark

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Copyright (c) arxde.ro
+// Copyright © 2024 ARXDE All Rights Reserved.
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
